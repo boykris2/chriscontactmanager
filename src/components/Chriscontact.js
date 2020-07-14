c08abd3b7b8cbbc7b12ac9c1a0af20883823c5ff
boykris2 <47758859+boykris2@users.noreply.github.com>
2020-07-14 23:53:10 +0100

    first commit

---
+++ src/components/Chriscontact.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Chriscontact extends Component {
+  render() {
+    return (
+      <div>
+        <h2>
+          Phone: 090-9014-3241 <br />
+          Email: chriskachi.okoro@gmail.com
+        </h2>
+      </div>
+    );
+  }
+}
+
+export default Chriscontact;

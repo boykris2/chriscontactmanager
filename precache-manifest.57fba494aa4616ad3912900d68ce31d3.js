d00e1e604b2c591ff4709e2136cb4f5144eaf90f
boykris2 <47758859+boykris2@users.noreply.github.com>
2020-07-15 00:01:27 +0100

    Updates

---
+++ precache-manifest.57fba494aa4616ad3912900d68ce31d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b470829dc9fdb2a4a285bdd86a6c5cd0",
+    "url": "/chriscontactmanager/index.html"
+  },
+  {
+    "revision": "f7985e7ee464881a1001",
+    "url": "/chriscontactmanager/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "224f317e2a9f0546f27f",
+    "url": "/chriscontactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "f7985e7ee464881a1001",
+    "url": "/chriscontactmanager/static/js/2.f77cce18.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/chriscontactmanager/static/js/2.f77cce18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "224f317e2a9f0546f27f",
+    "url": "/chriscontactmanager/static/js/main.ae99a547.chunk.js"
+  },
+  {
+    "revision": "c97f68a07b4b02ec2b35",
+    "url": "/chriscontactmanager/static/js/runtime-main.7d83db7b.js"
+  }
+]);
